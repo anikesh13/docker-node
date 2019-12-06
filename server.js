7c1c3865f270ae6c506421bdca21538ef73e8e35
Anikesh Dhanokar <anikesh13.linuxbean@gmail.com>
2019-12-06 12:35:05 +0530

    qwe

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const app = express();
-const port = process.env.APP_PORT || process.env.PORT;
+const port = process.env.PORT;
 const bodyParser = require('body-parser');
 // const restApiRoutes = require('./rest-api');
 // const graphqlRoutes = require('./graphql-api');
