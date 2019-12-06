d04335ab59c9efd70cc241cf0a06971c797b2a41
Anikesh Dhanokar <anikesh13.linuxbean@gmail.com>
2019-12-06 15:35:29 +0530

    edit-book

--- rest-api/controllers/bookCltr.js
+++ rest-api/controllers/bookCltr.js
@@ -1,6 +1,26 @@
 const bookSchema = require("../../models/bookSchema");
 
 module.exports = {
+    displayBook: function (req, res) {
+        bookSchema.find()
+            .then(result => {
+                res.status(200).json({
+                    message: 'Book List',
+                    success: true,
+                    status: 200,
+                    data: result
+                });
+            })
+            .catch(err => {
+                res.status(404).json({
+                    message: 'Book error',
+                    success: false,
+                    status: 404,
+                    data: err
+                });
+            })
+    },
+
     addBook: function (req, res) {
         const bookData = new bookSchema({
             ...req.body
@@ -15,22 +35,53 @@ module.exports = {
                 });
             })
             .catch(err => {
-                res.status(400).send(err);
+                res.status(404).json({
+                    message: 'Book error',
+                    success: false,
+                    status: 404,
+                    data: err
+                });
             });
     },
 
-    displayBook: function (req, res) {
-        bookSchema.find()
+    deleteBook: function (req, res) {
+        bookSchema.deleteOne({ _id: req.params.id })
             .then(result => {
                 res.status(200).json({
-                    message: 'Book List',
+                    message: 'Book Delete success',
                     success: true,
                     status: 200,
-                    data: result
                 });
             })
             .catch(err => {
-                res.send(err);
+                res.status(404).json({
+                    message: 'Book error',
+                    success: false,
+                    status: 404,
+                    data: err
+                });
+            });
+    },
+
+    editBook: function (req, res) {
+        bookSchema.updateOne({ _id: req.body._id },
+            {
+                ...req.body
             })
+            .then(result => {
+                res.status(200).json({
+                    message: 'Book Edited Successfully',
+                    success: true,
+                    status: 200,
+                });
+            })
+            .catch(err => {
+                res.status(404).json({
+                    message: 'Book error',
+                    success: false,
+                    status: 404,
+                    data: err
+                });
+            });
     }
 }
--- rest-api/routes/api/bookInfo/index.js
+++ rest-api/routes/api/bookInfo/index.js
@@ -2,7 +2,9 @@ const router = require("express").Router();
 
 // Crud book data
 const book = require("../../../controllers/bookCltr");
-router.route('/').post(book.addBook);
 router.route('/').get(book.displayBook);
+router.route('/').post(book.addBook);
+router.route('/').patch(book.editBook);
+router.route('/:id').delete(book.deleteBook);
 
 module.exports = router;
